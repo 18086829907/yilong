0f9968f4a5cb36fa5e559cb28c6946254ca16678
18086829907 <417217170@qq.com>
2019-08-31 09:30:10 +0800

    123

--- first_project/练习/scrapyTest/jdCrawl/jdCrawl/settings.py
+++ first_project/练习/scrapyTest/jdCrawl/jdCrawl/settings.py
@@ -104,7 +104,7 @@ HTTPCACHE_STORAGE = 'scrapy_splash.SplashAwareFSCacheStorage'
 
 SPLASH_URL = 'http://localhost:8050'
 
-PROXY_URL = 'http://112.195.47.177:8000/random'
+PROXY_URL = 'http://101.205.52.181:8000/random'
 
 SCHEDULER = 'scrapy_redis.scheduler.Scheduler'
 DUPEFILTER_CLASS = 'scrapy_redis.dupefilter.RFPDupeFilter'
