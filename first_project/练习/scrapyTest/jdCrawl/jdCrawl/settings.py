3efbca3f4a79a73d9db641186e48f3bfa2f3e887
18086829907 <417217170@qq.com>
2019-08-31 09:45:47 +0800

    123

--- first_project/练习/scrapyTest/jdCrawl/jdCrawl/settings.py
+++ first_project/练习/scrapyTest/jdCrawl/jdCrawl/settings.py
@@ -22,12 +22,12 @@ NEWSPIDER_MODULE = 'jdCrawl.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 3
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 10
+DOWNLOAD_DELAY = 15
 # The download delay setting will honor only one of:
 CONCURRENT_REQUESTS_PER_DOMAIN = 4
 CONCURRENT_REQUESTS_PER_IP = 4
