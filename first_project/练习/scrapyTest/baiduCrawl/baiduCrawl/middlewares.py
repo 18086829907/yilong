a8c26e6fe74a9a4861778f8ef4e61ece1d2be981
18086829907 <417217170@qq.com>
2019-09-01 18:55:04 +0800

    123

--- first_project/练习/scrapyTest/baiduCrawl/baiduCrawl/middlewares.py
+++ first_project/练习/scrapyTest/baiduCrawl/baiduCrawl/middlewares.py
@@ -6,7 +6,8 @@
 # https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
 from scrapy import signals
-
+import logging
+import requests
 
 class BaiducrawlSpiderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
@@ -103,3 +104,35 @@ class BaiducrawlDownloaderMiddleware(object):
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
+
+class ProxyUserAgentMiddleware():
+    def __init__(self, proxy_url):
+        self.logger = logging.getLogger(__name__)
+        self.proxy_url = proxy_url
+
+    def get_random_proxy(self):
+        try:
+            response=requests.get(self.proxy_url)
+            proxy = response.text
+            return proxy
+        except requests.ConnectionError:
+            return False
+
+    def process_request(self, request, spider):
+        # if request.meta.get('retry_times'):
+        #     proxy = self.get_random_proxy()
+        #     if proxy:
+        #         uri = 'https://{}'.format(proxy)
+        #         request.meta['splash']['args']['proxy'] = uri
+        #     userAgent = agent.allHeaders()
+        #     request.headers['User_Agent'] = userAgent
+        proxy = self.get_random_proxy()
+        uri = 'https://{}'.format(proxy)
+        request.meta['splash']['args']['proxy'] = uri
+        # userAgent = agent.allHeaders()
+        # request.headers['User_Agent'] = userAgent
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        settings = crawler.settings
+        return cls(proxy_url=settings.get('PROXY_URL'))
