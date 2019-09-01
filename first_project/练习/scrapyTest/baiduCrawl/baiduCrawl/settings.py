4eaf307df1bfeb2cd9fee7904ccdd17a35f8d2a1
18086829907 <417217170@qq.com>
2019-09-01 18:55:48 +0800

    123

--- first_project/练习/scrapyTest/baiduCrawl/baiduCrawl/settings.py
+++ first_project/练习/scrapyTest/baiduCrawl/baiduCrawl/settings.py
@@ -57,7 +57,8 @@ DOWNLOADER_MIDDLEWARES = {
     'scrapy_splash.SplashCookiesMiddleware':723,
     'scrapy_splash.SplashMiddleware':725,
     'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware':810,
-    'baiduCrawl.middlewares.ProxyUserAgentMiddleware': 543,
+    'baiduCrawl.middlewares.BaiducrawlDownloaderMiddleware': 543,
+    'baiduCrawl.middlewares.ProxyUserAgentMiddleware': 542,
 }
 
 # Enable or disable extensions
